fb1f2c842532c40f97ac6893c0f36643dbda39c9
Sergey-TR <s.trofimov72@gmail.com>
2022-06-16 19:25:10 +0300

    hw_cp

--- src/components/Messenger.jsx
+++ src/components/Messenger.jsx
@@ -1,61 +1,60 @@
 import React, {useState, useEffect} from "react";
-import MessengerList from "./MessengerList";
-
-const MESSAGE_BOT = 'Это сообщение от бота';
-const USER_NAME = 'author: user';
-const BOT_NAME = 'bot-message';
-const TIMEOUT = 1500;
-let timeoutId = null; 
 
 function Messenger() {
 
     const [messageList, setMessageList] = useState([]);
-    const [messageText, setMessageText] = useState('');
-
-    const sendMessage = () => {
-
-        if (messageText.trim().length === 0) {
-            alert('Вы пытаетесь отправить пустое сообщение!!! Оно не будет отправлено.');
-            return;
-        }
-
-        setMessageList([...messageList, {
-            text: messageText,
-            author: USER_NAME
-        }]);
-
-        setMessageText('');
-
-    };
-
-    const sendMessageBot = () => {
-
-        setMessageList([...messageList, {
-            text: MESSAGE_BOT,
-            author: BOT_NAME
-        }]);
-    };
+    const [content, setContent] = useState('');
+    const [author, setAuthor] = useState('');
 
     useEffect(() => {
-        if (messageList.length === 0) return;
-        if (messageList[messageList.length - 1].author === 'bot-message') return;
+        setTimeout(() => {
+            botAnswer(messageList)
+        }, 1500)
+    }, [messageList])
 
-        timeoutId = setTimeout(sendMessageBot, TIMEOUT);
+    const submitHandler = (event) => {
+        event.preventDefault();
 
-        return () => clearTimeout(timeoutId);
-    }, [messageList]);
+        setMessageList(prevState => [...prevState, {
 
-    const inputMessageHandler = ({target}) => {setMessageText(target.value)};
+            id: getLastId(prevState),
+            content,
+            author
+
+        }])
+    }
+
+    function getLastId(arr) {
+        return arr.length ? arr[arr.length - 1].id + 1 : 0;
+    }
+
+    function botAnswer() {
+        const lastAuthor = messageList[messageList.length - 1];
+        if(lastAuthor && lastAuthor.author) {
+            setMessageList(prevState => [...prevState, {
+                id: getLastId(prevState),
+                content: `Сообщение автора ${lastAuthor.author} отправлено`
+            }])
+        }
+    }
 
     return (
-        <div className="wrapper">
-            <MessengerList messages={messageList} userName={USER_NAME}/>
-            <div className="messageForm">
-                <textarea className="txt-area" value={messageText} onChange={inputMessageHandler}></textarea>
-                <button onClick={sendMessage} className="btn btn-outline-primary">SEND</button>
-            </div>
+        <div>
+            {messageList.map((message) => {
+                return (
+                    <div key={message.id}>
+                        {message.author && <p>Автор: {message.author}</p>}
+                        <p>{message.author && <span>Текст:</span>} {message.content}</p>
+                    </div>
+                )
+            })}
+            <form onSubmit={submitHandler}>
+                <input value={content} onChange={(event) => setContent(event.target.value)}/>
+                <input value={author} onChange={(event) => setAuthor(event.target.value)}/>
+                <button type="submit">SEND</button>
+            </form>
         </div>
-    );
+    )
 
 }
 
