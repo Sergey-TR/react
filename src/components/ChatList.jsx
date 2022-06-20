daacc77bc42a05a0f38293d7cb0629423eccc57c
Sergey-TR <s.trofimov72@gmail.com>
2022-06-20 08:13:32 +0300

    install material ui

---
+++ src/components/ChatList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ChatList () {
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ChatList;
--- src/components/Messenger.jsx
+++ src/components/Messenger.jsx
@@ -1,10 +1,34 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import ChatList from "./ChatList";
+import {
+    Button,
+    Box,
+    TextField,
+    List,
+    Typography,
+    ThemeProvider,
+    createTheme,
+} from "@mui/material";
+import { blue } from "@mui/material/colors";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: blue[100],
+        }
+    },
+});
 
 function Messenger() {
 
     const [messageList, setMessageList] = useState([]);
     const [content, setContent] = useState('');
     const [author, setAuthor] = useState('');
+    const [list] = useState([
+        { name: 'Vue', id: '1' },
+        { name: 'React', id: '2' },
+        { name: 'Nodejs', id: '3' }
+    ]);
 
     useEffect(() => {
         setTimeout(() => {
@@ -30,7 +54,7 @@ function Messenger() {
 
     function botAnswer() {
         const lastAuthor = messageList[messageList.length - 1];
-        if(lastAuthor && lastAuthor.author) {
+        if (lastAuthor && lastAuthor.author) {
             setMessageList(prevState => [...prevState, {
                 id: getLastId(prevState),
                 content: `Сообщение автора ${lastAuthor.author} отправлено`
@@ -39,20 +63,34 @@ function Messenger() {
     }
 
     return (
-        <div>
-            {messageList.map((message) => {
-                return (
-                    <div key={message.id}>
-                        {message.author && <p>Автор: {message.author}</p>}
-                        <p>{message.author && <span>Текст:</span>} {message.content}</p>
-                    </div>
-                )
-            })}
-            <form onSubmit={submitHandler}>
-                <input value={content} onChange={(event) => setContent(event.target.value)}/>
-                <input value={author} onChange={(event) => setAuthor(event.target.value)}/>
-                <button type="submit">SEND</button>
-            </form>
+        <div className="wrapper">
+
+            <div className="wrapper-box">
+                <div className="chat-box">
+                    <Typography variant="h5" component="div" color="primary">CHATS</Typography>
+                    <ChatList>
+
+                    </ChatList>
+                </div>
+                <div className="messenger-box">
+                   <div className="messenger-content">
+                   {messageList.map((message) => {
+                        return (
+                            <div key={message.id}>
+                                {message.author && <p>Автор: {message.author}</p>}
+                                <p>{message.author && <span>Текст:</span>} {message.content}</p>
+                            </div>
+                        )
+                    })}
+                   </div>
+                    <form onSubmit={submitHandler}>
+                        <input value={content} onChange={(event) => setContent(event.target.value)} />
+                        <input value={author} onChange={(event) => setAuthor(event.target.value)} />
+                        <Button type="submit" variant="contained">SEND</Button>
+                    </form>
+                </div>
+            </div>
+
         </div>
     )
 
